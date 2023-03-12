657d1ad89643e53d4c6331ded8c99bd9cd79a880
Vasyl Kononenko <vakomail11@gmail.com>
2023-03-12 21:31:54 +0200

    fix some details

--- hw2.py
+++ hw2.py
@@ -1,21 +1,27 @@
-member_name = input('Enter your name(example "Name Surname"): ')
+member_name = ''
 list_members = {}
 
 while member_name != 'q':
-    list_members[member_name] = 0
     member_name = input('Enter your name(example "Name Surname"): ')
-    if member_name in list_members:
-        print('name already in list')
-        other_name = input('Enter a unique name. You can use numbers at the end of the name: ')
-        list_members[other_name] = 0
+    if member_name == 'q':
+        break
+    elif member_name in list_members:
+        print('The name already in list, enter new name.')
+        continue
+    list_members[member_name] = 0
+    print('For quit enter "q".')
 
 print('-' * 100)
 
-for i in list_members:
-    selected_name = input(f'{i}, choose who you want to vote for(example "Name Surname") {list_members}: ')
+counter = 0
+while counter != len(list_members):
+    selected_name = input(f'{list(list_members.keys())[counter]} '
+                          f'choose who you want to vote for(example "Name Surname") {list_members}: ')
     if selected_name not in list_members:
-        selected_name = input(f'Choose correct who you want to vote for(example "Name Surname") {list_members}: ')
+        print(f'{selected_name} not in list. Choose correct name {list_members}: ')
+        continue
     list_members[selected_name] += 1
+    counter += 1
 
 max_val = max(list_members.values())
 
