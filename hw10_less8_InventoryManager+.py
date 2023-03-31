e6691e474394d7919bcbd30ffd78058df3a39cbf
Vasyl <vasyl@Air-Vasyl.local>
2023-03-31 15:59:53 +0200

    complete hw10

--- hw10_less8_InventoryManager+.py
+++ hw10_less8_InventoryManager+.py
@@ -5,68 +5,93 @@ inventory = {}
 
 
 def search_inv():
-    search_item = input('Enter item or description: ')
-    result = []
-    for i, d in inventory.items():
-        if search_item.lower() in i.lower() or search_item.lower() in d['description'].lower():
-            result.append(i)
-    if result:
-        for j in result:
+    try:
+        search_item = input('Enter item or description: ')
+        result = []
+        for i, d in inventory.items():
+            if search_item.lower() in i.lower() or search_item.lower() in d['description'].lower():
+                result.append(i)
+        if result:
+            for j in result:
+                print('*' * 200)
+                print(f"{j}({inventory[j]['description']}). Quantity: {inventory[j]['quantity']}")
+                print('*' * 200)
+        else:
             print('*' * 200)
-            print(f"{j}({inventory[j]['description']}). Quantity: {inventory[j]['quantity']}")
+            print('Item or description not found.')
             print('*' * 200)
-    else:
+    except Exception as e:
         print('*' * 200)
-        print('Item or description not found.')
+        print(f"Error occurred while searching inventory: {e}")
         print('*' * 200)
 
 
 def updt_quantity():
-    name_item = input('Enter the item: ')
-    if name_item in inventory:
-        updt_q = int(input('Enter new quantity of item: '))
-        inventory[name_item]['quantity'] = updt_q
+    try:
+        name_item = input('Enter the item: ')
+        if name_item in inventory:
+            updt_q = int(input('Enter new quantity of item: '))
+            inventory[name_item]['quantity'] = updt_q
+            print('*' * 200)
+            print(f'Successfully updated quantity')
+            print('*' * 200)
+        else:
+            print('*' * 200)
+            print('Item not found')
+            print('*' * 200)
+    except Exception as e:
         print('*' * 200)
-        print(f'Successfully updated quantity')
-        print('*' * 200)
-    else:
-        print('*' * 200)
-        print('Item not found')
+        print(f"Error occurred while updating quantity: {e}")
         print('*' * 200)
 
 
 def remove_inv():
-    remove = input('Enter item you want to remove from inventory: ')
-    if remove in inventory:
-        del inventory[remove]
+    try:
+        remove = input('Enter item you want to remove from inventory: ')
+        if remove in inventory:
+            del inventory[remove]
+            print('*' * 200)
+            print(f'Successfully deleted {remove}')
+            print('*' * 200)
+        else:
+            print('*' * 200)
+            print('Item not found.')
+            print('*' * 200)
+    except Exception as e:
         print('*' * 200)
-        print(f'Successfully deleted {remove}')
-        print('*' * 200)
-    else:
-        print('*' * 200)
-        print('Item not found.')
+        print(f"Error occurred while removing item: {e}")
         print('*' * 200)
 
 
 def add_inv():
-    item = input('Enter item: ')
-    description = input('Enter a description of the item: ')
-    quantity = int(input('Enter quantity of items: '))
-    inventory[item] = {'description': description, 'quantity': quantity}
-    print('*' * 200)
-    print('Successfully added.')
-    print('*' * 200)
+    try:
+        item = input('Enter item: ')
+        description = input('Enter a description of the item: ')
+        quantity = int(input('Enter quantity of items: '))
+        inventory[item] = {'description': description, 'quantity': quantity}
+        print('*' * 200)
+        print('Successfully added.')
+        print('*' * 200)
+    except Exception as e:
+        print('*' * 200)
+        print(f"Error occurred while adding item: {e}")
+        print('*' * 200)
 
 
 def view_inv():
-    if inventory == {}:
+    try:
+        if inventory == {}:
+            print('*' * 200)
+            print('Empty inventory. Press "a" to add item to your inventory.')
+            print('*' * 200)
+        else:
+            print('*' * 200)
+            for item, details in inventory.items():
+                print(f"{item}({details['description']}). Quantity: {details['quantity']}")
+            print('*' * 200)
+    except Exception as e:
         print('*' * 200)
-        print('Empty inventory. Press "a" to add item to your inventory.')
-        print('*' * 200)
-    else:
-        print('*' * 200)
-        for item, details in inventory.items():
-            print(f"{item}({details['description']}). Quantity: {details['quantity']}")
+        print(f"Error occurred while viewing inventory: {str(e)}")
         print('*' * 200)
 
 
