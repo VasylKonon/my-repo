5736370f5492a0538806dde07ad6a97137918bc0
VasylKonon <vakomail11@gmail.com>
2023-06-08 21:27:48 +0200

    fix

--- ITb_hw3.py
+++ ITb_hw3.py
@@ -1,14 +1,10 @@
-try:
-    number1 = int(input('Enter first number: '))
-except ValueError:
-    print("Entered not a number.")
-else:
-    print(f"You first number is {number1}")
+def is_error():
+    try:
+        number1 = int(input('Enter first number: '))
+        number2 = int(input('Enter second number: '))
+        return f"Your first number is {number1}, your second number is {number2}"
+    except ValueError:
+        return "Entered not a number."
 
 
-try:
-    number2 = int(input('Enter second number: '))
-except ValueError:
-    print("Entered not a number.")
-else:
-    print(f"You second number is {number2}")
+print(is_error())
