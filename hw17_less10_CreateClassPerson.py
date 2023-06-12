1c18cde705d4f4c998be145f5cd4310d54821c13
VasylKonon <vakomail11@gmail.com>
2023-06-12 14:38:29 +0200

    fix

--- hw17_less10_CreateClassPerson.py
+++ hw17_less10_CreateClassPerson.py
@@ -6,21 +6,34 @@ class Person:
         self.dog = None
 
     def __str__(self):
-        return f"{self.name}, {self.age}"
+        return f"Name: {self.name}, age: {self.age}"
 
     def add_dog(self, dog):
-        self.dog = dog
-
-    def bark(self, dog):
-        if not isinstance(dog, Person):
-            raise TypeError("Invalid input. Expected Dog instance.")
-        if self.dog:
-            print(f"{self.dog} bark")
+        if isinstance(dog, Dog):
+            self.dog = dog
         else:
-            print("No dog added yet!")
+            raise ValueError("Invalid input of dog")
+
+    def bark(self):
+        if self.dog is not None:
+            self.dog.bark()
+        else:
+            return f"{self.name} doesn't have a dog."
+
+
+class Dog:
+    def __init__(self, name, breed, age):
+        self.name = name
+        self.breed = breed
+        self.age = age
+
+    def bark(self):
+        return f"The dog's name is {self.name} and the breed is {self.breed}"
 
 
 person1 = Person("John", 30, "Male")
-person1.add_dog("Labrador")
+dog1 = Dog("Max", "Labrador", 3)
+person1.add_dog(dog1)
 
-person1.bark(person1)
+print(person1)
+print(person1.bark())
