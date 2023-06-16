4f8a6cccc74f015085b8cb1982e4f667be8a41de
VasylKonon <vakomail11@gmail.com>
2023-06-16 16:09:52 +0200

    complete hw39

---
+++ ITb_m2_less6_hw39.py
@@ -0,0 +1,35 @@
+class Shape:
+    def __init__(self, color):
+        self.color = color
+
+    def display_color(self):
+        return f"Color: {self.color}"
+
+
+class Rectangle(Shape):
+    def __init__(self, color, width, height):
+        super().__init__(color)
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def get_width(self):
+        return self.width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_height(self):
+        return self.height
+
+    def display_color(self):
+        return f"Color: {self.color}, width: {self.width}, height: {self.height}"
+
+
+shape = Shape("Red")
+print(shape.display_color())
+
+rectangle = Rectangle("Blue", 10, 5)
+print(rectangle.display_color())
